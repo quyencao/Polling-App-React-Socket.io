5ccb0346b09de3632ec3a1adbdba2eae88ac49ac
quyencao <boy20195@gmail.com>
2017-03-30 23:36:24 +0700

    Config app

---
+++ components/App.js
@@ -0,0 +1,11 @@
+const React = require('react');
+
+var App = React.createClass({
+    render() {
+        return (
+            <h1>Hello word from React 2</h1>
+        )
+    }
+});
+
+module.exports = App;
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    entry: './app-client.js',
+    output: {
+       filename: 'public/bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                exclude: /(node_modules|app-server.js)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015', 'stage-0']
+                }
+            }
+        ]
+    }
+};
